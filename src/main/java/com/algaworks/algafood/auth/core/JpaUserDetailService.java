9122ff051d2adfe124de0aba3f29fbd3cfa694fb
Robson Martinhão <robmartinhao@gmail.com>
2022-11-04 20:12:36 -0300

    23.14. Autenticando usuário com dados do banco de dados

---
+++ src/main/java/com/algaworks/algafood/auth/core/JpaUserDetailService.java
@@ -0,0 +1,22 @@
+package com.algaworks.algafood.auth.core;
+
+import com.algaworks.algafood.auth.domain.Usuario;
+import com.algaworks.algafood.auth.domain.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JpaUserDetailService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado com o e-mail informado"));
+        return new AuthUser(usuario);
+    }
+}
---
+++ src/main/java/com/algaworks/algafood/auth/domain/Usuario.java
@@ -0,0 +1,26 @@
+package com.algaworks.algafood.auth.domain;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@EqualsAndHashCode
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @JoinColumn(nullable = false)
+    private String nome;
+
+    @JoinColumn(nullable = false)
+    private String email;
+
+    @JoinColumn(nullable = false)
+    private String senha;
+}
